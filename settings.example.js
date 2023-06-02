42044497dfcab22ff4e9411d6a18fef496b58f67
qwqVictor <i@qwq.ren>
2023-06-02 14:19:09 +0800

    docs(settings): chatgpt browser client token

--- settings.example.js
+++ settings.example.js
@@ -61,7 +61,7 @@ export default {
         // Warning: This will expose your access token to a third party. Consider the risks before using this.
         reverseProxyUrl: 'https://bypass.churchless.tech/api/conversation',
         // Access token from https://chat.openai.com/api/auth/session
-        accessToken: '',
+        accessToken: async () => { return '' },
         // Cookies from chat.openai.com (likely not required if using reverse proxy server).
         cookies: '',
         // A proxy string like "http://<ip>:<port>"
